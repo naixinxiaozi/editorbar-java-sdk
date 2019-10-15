fd7baabc9ed451995ed7128335a47a4507fb4cfb
Andy <zhangke002@foxmail.com>
2019-10-15 18:03:36 +0800

    ignore ssl certs

--- src/main/java/com/editorbar/sdk/Client.java
+++ src/main/java/com/editorbar/sdk/Client.java
@@ -7,7 +7,9 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import javax.net.ssl.*;
 import java.io.IOException;
+import java.security.SecureRandom;
 import java.util.concurrent.TimeUnit;
 
 public class Client {
@@ -56,11 +58,33 @@ public class Client {
 
     private void initHttpClient(Config config) {
         ConnectionPool connectionPool = new ConnectionPool(config.getMaxIdleConnections(), config.getKeepAliveDuration(), TimeUnit.MINUTES);
+
+        TrustAllManager trustAllManager = new TrustAllManager();
         conn = new OkHttpClient.Builder()
                 .connectTimeout(config.getConnectTimeout(), TimeUnit.SECONDS)
                 .readTimeout(config.getReadTimeout(), TimeUnit.SECONDS)
                 .writeTimeout(config.getWriteTimeout(), TimeUnit.SECONDS)
                 .connectionPool(connectionPool)
+                .sslSocketFactory(createTrustAllSSLFactory(trustAllManager), trustAllManager)
+                .hostnameVerifier(createTrustAllHostnameVerifier())
                 .build();
     }
+
+    private SSLSocketFactory createTrustAllSSLFactory(TrustAllManager trustAllManager) {
+        SSLSocketFactory ssfFactory = null;
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, new TrustManager[]{trustAllManager}, new SecureRandom());
+            ssfFactory = sc.getSocketFactory();
+        } catch (Exception ignored) {
+            ignored.printStackTrace();
+        }
+
+        return ssfFactory;
+    }
+
+    private HostnameVerifier createTrustAllHostnameVerifier() {
+        return (hostname, session) -> true;
+    }
+
 }
